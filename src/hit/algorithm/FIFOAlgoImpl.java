04a8b36cba7d0eeb06bf6e879d12e0f846c81379
oreniko26 <coheoren@gmail.com>
2016-04-03 13:25:00 +0300

    FIFO Implementation

--- src/hit/algorithm/FIFOAlgoImpl.java
+++ src/hit/algorithm/FIFOAlgoImpl.java
@@ -1,33 +1,46 @@
 package hit.algorithm;
 
 import java.util.*;
+import java.util.Map.Entry;
 
-public class FIFOAlgoImpl<K,V> implements IAlgoCache<K,V> {
+public class FIFOAlgoImpl<K,V> extends LinkedHashMap<K,V> implements IAlgoCache<K,V> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int capacity;
+	private LinkedHashMap<K,V> map;
 	
 	public FIFOAlgoImpl(int capacity){
 		
 		this.capacity = capacity;
+		map = new LinkedHashMap<K, V>(this.capacity);
 		
 	}
 	@Override
 	public V getElement(K key) {
 		
-		return getElement(key);
+		return map.get(key);
 	}
 
 	@Override
 	public V putElement(K key, V value) {
 		
-		return putElement(key, value);
+		return map.put(key, value);
+	
 	}
 
 	@Override
 	public void removeElement(K key) {
 		
-		removeElement(key);
+		map.remove(key);
 	}
+	
+	 @Override
+	public boolean removeEldestEntry(Entry<K, V> entry) {
+	    return size() > this.capacity;
+	  }
 	
 	
 }
