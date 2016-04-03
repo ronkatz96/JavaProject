2e204eceb70b897345066166a3c5a7ca52939099
ronkatz96 <ron.katzzz@gmail.com>
2016-04-03 14:42:15 +0300

    HD changes

--- src/hit/memoryunits/HardDisk.java
+++ src/hit/memoryunits/HardDisk.java
@@ -1,5 +1,30 @@
 package hit.memoryunits;
+import java.io.*;
 
 public class HardDisk {
 
+	static final String DEFAULT_FILE_NAME = "hdPages.txt";
+	static final int _SIZE = 1000;
+	private static final HardDisk instance = new HardDisk();
+	
+	private HardDisk(){
+		
+	}
+	
+	private void writeHd() throws FileNotFoundException, IOException{
+		
+	}
+	
+	public static HardDisk getInstance(){
+		
+		return instance;
+	}
+	
+	public Page<byte[]> pageFault(Long pageId) throws FileNotFoundException, IOException{
+		
+	}
+	
+	public Page<byte[]> pageReplacement(Page<byte[]> moveToHdPage, Long moveToRamId)throws FileNotFoundException, IOException{
+		
+	}
 }
