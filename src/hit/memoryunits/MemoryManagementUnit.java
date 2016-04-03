ed4b08c3f6d6c989d161224b47c7747dca271d60
oreniko26 <coheoren@gmail.com>
2016-04-03 14:44:32 +0300

    MMU Initial Implementation

--- src/hit/memoryunits/MemoryManagementUnit.java
+++ src/hit/memoryunits/MemoryManagementUnit.java
@@ -1,5 +1,41 @@
 package hit.memoryunits;
 
-public class MemoryManagementUnit {
+//import java.lang.reflect.Array;
+//import java.util.Collection;
+//import java.util.List;
 
+import hit.algorithm.IAlgoCache;
+
+public class MemoryManagementUnit {
+	
+	private IAlgoCache<java.lang.Long,java.lang.Long> algo;
+	private RAM ram;
+	
+	public MemoryManagementUnit(int ramCapacity, IAlgoCache<java.lang.Long,java.lang.Long> algo) 
+	{
+		this.ram = new RAM(ramCapacity);
+		this.algo = algo;
+	}
+	
+	public IAlgoCache<java.lang.Long,java.lang.Long> getAlgo()
+	{
+		return algo;
+	}
+	
+	public RAM getRam() 
+	{
+		return ram;
+	}
+	
+	public void setRam(RAM ram) 
+	{
+		this.ram = ram;
+	}
+	
+	public Page<byte[]>[] getPages(java.lang.Long[] pageIds)
+	{
+		return null;
+		
+		
+	}
 }
